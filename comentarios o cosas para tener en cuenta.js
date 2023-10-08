49f4b684c0c2f2ceb9171f2cc00dcaf7909105a3
NachoGaynor2003 <nachoteam14@gmail.com>
2023-10-08 16:33:21 -0300

    Updated

---
+++ comentarios o cosas para tener en cuenta.js
@@ -0,0 +1,23 @@
+/*console.log(
+            "\n------------------------------------------------------\n"
+          );
+          console.log(`Título: ${tareas[i].titulo}`);
+          console.log(`Descripción: ${tareas[i].descripcion}`);
+          console.log(`Estado: ${tareas[i].estado}`);
+          console.log(
+            `Fecha de creación: ${tareas[i].fechaCreacion.toLocaleDateString()}`
+          );
+          console.log(`Fecha de vencimiento: ${tareas[i].fechaVencimiento}`);
+          console.log(`Dificultad: ${tareas[i].dificultad}`);
+          console.log(
+            "\n------------------------------------------------------"
+          );*/
+
+/*const tarea = {
+        titulo: "Tarea 1",
+        descripcion: "Descripcion de la Tarea 1",
+        estado: "pendiente",
+        fechaCreacion: new Date(),
+        fechaVencimiento: null,
+        dificultad: 0,
+      };*/
